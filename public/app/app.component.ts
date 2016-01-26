a92e2e5bb969350ac6ba203563b3bce0dee31e5f
dabutvin <dabutvin@microsoft.com>
2016-01-26 01:23:03 -0800

    adds textbox filter

--- public/app/app.component.ts
+++ public/app/app.component.ts
@@ -1,12 +1,36 @@
 import {Component} from 'angular2/core';
 import {Items} from './items';
+import {FilterTextboxComponent} from './filter-textbox-component';
 
 @Component({
 	selector: 'morty-crafts',
+	directives: [FilterTextboxComponent],
 	templateUrl: 'html/index.html'
 })
 
 export class AppComponent { 
 	word = 'asdas';
 	items = new Items().data;
+	filteredItems = this.items;
+
+	filterChanged(data: string) {
+		if(data) {
+			data = data.toUpperCase();
+			let props = ['name', 'desc'];
+			this.filteredItems = this.items.filter( item => {
+				let match = false;
+				for (let prop of props)	{
+
+					if(item[prop].toString().toUpperCase().indexOf(data) > -1) {
+						match = true;
+						break;
+					}
+				}
+
+				return match;
+			});
+		} else {
+			this.filteredItems = this.items;
+		}
+	}
 }
