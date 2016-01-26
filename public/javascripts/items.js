321583254d5b016b48efe81671cc9807d570e425
dabutvin <dabutvin@microsoft.com>
2016-01-25 22:56:20 -0800

    add full item set

--- public/javascripts/items.js
+++ public/javascripts/items.js
@@ -1,13 +1,101 @@
 (function(app){
 
 	app.items = [
-		{
-			name: 'Cable',
-			desc: 'A cable'
-		},
-		{
-			name: 'Morty Manipulator Chip',
-			desc: 'Controls a Morty'
-		}
+	{
+		name: 'Morty Manipulator Chip',
+		desc: 'Captures a wild Morty with low health.',
+		price: 500
+	},
+	{
+		name: 'Serum',
+		desc: 'Heals 20 HP.',
+		price: 250
+	},
+	{
+		name: 'Great Serum',
+		desc: 'Heals 50 HP.',
+		price: 500
+	},
+	{
+		name: 'Sensational Serum',
+		desc: 'Heals 200 HP.',
+		price: 1200
+	},
+	{
+		name: 'Pure Serum',
+		desc: 'Heals all HP.',
+		price: 1500
+	},
+	{
+		name: 'Plutonic Rock',
+		desc: 'Adds 50% of AP to all attacks.',
+		price: 200
+	},
+	{
+		name: 'Pure Plutonic Rock',
+		desc: 'Restores full AP to all attacks.',
+		price: 800
+	},
+	{
+		name: 'Halzinger',
+		desc: 'Restores a dazed Morty to 50% HP.',
+		price: 650
+	},
+	{
+		name: 'Pure Halzinger',
+		desc: 'Restores a dazed Morty to 100% HP.',
+		price: 1800
+	},
+	{
+		name: 'Attack Mega Seed',
+		desc: 'Increase Attack stat.',
+		price: 1000
+	},
+	{
+		name: 'Defense Mega Seed',
+		desc: 'Increase Defense stat.',
+		price: 1000
+	},
+	{
+		name: 'Speed Mega Seed',
+		desc: 'Increase Speed stat.',
+		price: 1000
+	},
+	{
+		name: 'Level Up Mega Seed',
+		desc: 'Increases a Mortys level by 1.'
+	},
+	{
+		name: 'Mr. Meeseeks Box',
+		desc: 'Instantly defeats another Morty.'
+	},
+	{
+		name: 'Battery',
+		desc: 'You can never have enough of these.'
+	},
+	{
+		name: 'Fleeb',
+		desc: 'Excretes Fleeb Juice, a magical and versatile fluid.'
+	},
+	{
+		name: 'Bacteria Cell',
+		desc: 'A life form that can manipulate its host.'
+	},
+	{
+		name: 'Circuit Board',
+		desc: 'The heart of consumer electronics.'
+	},
+	{
+		name: 'Tin Can',
+		desc: 'Bend and reformed for a variety of purposes.'
+	},
+	{
+		name: 'Cable',
+		desc: 'Just your standard bit of red cable.'
+	},
+	{
+		name: 'Turbulent Juice Tube',
+		desc: 'Apply generously to make things purer, stronger.'
+	}
 	];
 })(window.app || (window.app = {}));
